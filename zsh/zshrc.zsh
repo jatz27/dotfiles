1885bfe0f1e8c1b1979d9f7a7de5f6d11fad3c3b
Julio Atz <julioatz90@gmail.com>
2023-09-24 03:26:13 -0600

    Icons and path
    Change path of alpha and add path homebrew

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -2,3 +2,5 @@ source ~/.config/zsh/conf/settings.zsh
 source ~/.config/zsh/conf/alias.zsh
 source ~/.config/zsh/conf/shortcuts.zsh
 source ~/.config/zsh/conf/plugins.zsh
+# Path for homebrew config
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
